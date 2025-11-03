928f1d3a6ccb56a37225a820940232324bd4d851
algoga <algoga@alumnatflorida.es>
2025-11-03 09:27:00 +0100

    "Ejercicio 1 finalizado"

--- 1.php
+++ 1.php
@@ -1,1 +1,12 @@
 <?php
+
+function diferenciaEdad($edad1, $edad2){
+    if ($edad1>$edad2){
+        $comparacion=$edad1-$edad2;
+    } else {
+        $comparacion=$edad2-$edad1;
+    }
+    return $comparacion;
+}
+$diferencia=diferenciaEdad(10,19);
+echo "La diferencia de edad es de $diferencia año/s";
