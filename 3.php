500de5d5fb88b28447266b846dddf6d446596187
algoga <algoga@alumnatflorida.es>
2025-11-03 10:19:06 +0100

    Ejercicio 3 finalizado

--- 3.php
+++ 3.php
@@ -1,1 +1,21 @@
 <?php
+$numero=dameNumero(6);
+function dameNumero($num){
+    $totalLote=[];
+    for ($i=0;$i<$num;$i++){
+        $lote=rand(1,49);
+        $visto=false;
+        for ($j=0;$j<count($totalLote);$j++){
+            if ($lote==$totalLote[$j]){
+            $visto=true;
+        }
+    }
+        if ($visto==true){
+            $i--;
+        } else {
+            $totalLote[]=$lote;
+            echo " $lote ";
+        }
+    }
+}
+echo $numero;
