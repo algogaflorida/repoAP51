6af19eb37de6ae4c56133f97af82030101863e44
algoga <algoga@alumnatflorida.es>
2025-11-03 09:51:42 +0100

    Ejercicio 2 hecho

--- 2.php
+++ 2.php
@@ -1,1 +1,7 @@
 <?php
+function numRomano($numero){
+    $numerosRomanos=["Nada", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+    return $numerosRomanos[$numero];
+}
+$num=numRomano(10);
+echo $num;
